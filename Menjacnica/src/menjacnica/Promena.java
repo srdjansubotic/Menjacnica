0c6b3855b9c72dd0c8bd082bdd40bb852a255b1c
srdjansubotic <srdjansubotic@yahoo.com>
2016-03-15 13:56:04 +0100

    Napravljena nova klasa Promena, koja implementira interfejs Menjacnica.

---
+++ Menjacnica/src/menjacnica/Promena.java
@@ -0,0 +1,28 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+import menjacnicaInterfejs.Menjacnica;
+
+public class Promena implements Menjacnica {
+
+	
+	public void dodavanjeKursaValute(String naziv, String skraceniNaziv, GregorianCalendar datum, double srednjiKurs,
+			double kupovniKurs, double prodajniKurs) {
+		
+
+	}
+
+	
+	public void brisanjeKursaValute(String naziv, String skraceniNaziv, GregorianCalendar datum) {
+		
+
+	}
+
+	
+	public Valuta pronadjiKurs(String naziv, String skraceniNaziv, GregorianCalendar datum) {
+		
+		return null;
+	}
+
+}
